16f6a924bcf307681ddf100ad3ee2a3b1cfbc697
Ajay v <vajay4834@gmail.com>
2023-05-29 11:52:19 +0530

    Made list adding functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import {useState} from 'react';
 
 function App() {
-  const [toDos,setTodos]=useState([])
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  const addTodo = () => {
+    if (toDo.trim() !== '') {
+      setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+      setToDo('');
+    }
+  };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -14,19 +22,31 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={addTodo} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+        {toDos.map((value) => (
+          <div className="todo" key={value.id}>
+            <div className="left">
+              <input
+                type="checkbox"
+                checked={value.status}
+                name=""
+                id=""
+              />
+              <p>{value.text}</p>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
+            </div>
           </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
