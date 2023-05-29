02e8ee0fbe9ec1cdd277eb4e7ec8eff2582278b5
Ajay v <vajay4834@gmail.com>
2023-05-29 13:23:12 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -7,19 +7,51 @@ function App() {
 
   const addTodo = () => {
     if (toDo.trim() !== '') {
-      setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+      const currentDate = new Date();
+      const dateTimeString = currentDate.toLocaleString();
+      const newTodo = {
+        id: Date.now(),
+        text: toDo,
+        status: false,
+        dateTime: dateTimeString,
+      };
+      setToDos([...toDos, newTodo]);
       setToDo('');
     }
   };
+  
+  const handleCheckboxChange = (id, checked) => {
+    setToDos((prevToDos) =>
+      prevToDos.map((todo) =>
+        todo.id === id ? { ...todo, status: checked } : todo
+      )
+    );
+  };
+
+  const handleDeleteTodo = (id) => {
+    setToDos((prevToDos) => prevToDos.filter((todo) => todo.id !== id));
+  };
+
+  const formatTime = (dateTimeString) => {
+    const date = new Date(dateTimeString);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')}`;
+    return formattedTime;
+  };
+  
+  
 
   return (
     <div className="app">
       <div className="mainHeading">
-        <h1>ToDo List</h1>
+        <h1>Ajay,'s ToDo List</h1>
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, it's {new Date().toLocaleDateString('en-US', { weekday: 'long' })} 🌝 ☕</h2>
       </div>
       <div className="input">
         <input
@@ -31,18 +63,23 @@ function App() {
         <i onClick={addTodo} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        {toDos.map((value) => (
-          <div className="todo" key={value.id}>
+        {toDos.map((obj) => (
+          <div className={`todo ${obj.status ? 'completed' : ''}`} key={obj.id}>
             <div className="left">
               <input
                 type="checkbox"
-                checked={value.status}
-                name=""
-                id=""
+                checked={obj.status}
+                onChange={(e) =>
+                  handleCheckboxChange(obj.id, e.target.checked)
+                }
               />
-              <p>{value.text}</p>
+              <p>{obj.text}</p>
+              <p className="datetime">{formatTime(obj.dateTime)}</p>
               <div className="right">
-                <i className="fas fa-times"></i>
+                <i
+                  className="fas fa-times"
+                  onClick={() => handleDeleteTodo(obj.id)}
+                ></i>
               </div>
             </div>
           </div>
